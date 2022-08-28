e3b6c52f3ec2bee1957f0a8f398b1390331d189f
TheMrArthur <arturgorlov5@gmail.com>
2022-08-28 20:14:00 +0300

    Initial Commit

---
+++ iOSSettingsScreen HW13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSSettingsScreen HW13
+//
+//  Created by Артур Горлов on 28.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
